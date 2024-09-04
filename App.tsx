455bc7a15a490f95ee52c981c0c2767e940919c1
sousa-gabriel <sousa-gabriel@hotmail.com>
2024-09-04 14:15:10 -0300

    configuration `fonts this app`

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { SignIn } from "./src/screens/SignIn";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
-
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { LoadIndicator } from "./src/components/Loading/styles";
+import { StatusBar } from "react-native";
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <LoadIndicator />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <SignIn />
     </ThemeProvider>
   );
